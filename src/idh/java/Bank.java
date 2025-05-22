fe00448fb8d4473992b1675ab9dc80a0a91d3fd1
Yuima <Yuima@172.29.209.62>
2025-05-22 17:13:12 +0200

    Frau Officer! Ich möchte ein Verbrechen reporten! Ich habe Aufgabe 1 gelöst und hochgeladen, und mir wurde mein Herz gestohlen! Java hat mein Herz gestohlen! (kmao)

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,37 +1,35 @@
 package idh.java;
 
+import java.util.HashMap;
+
 public class Bank{
+
+	
 	
 	// TODO: Use Map to enable a higher number of accounts 
 	// and start nextAccountID with 7000001 
-	Account[] accounts = new Account[5];
+	HashMap<Integer, Account> accounts = new HashMap<Integer, Account>();
 	int nextAccountID;
 
 	public Bank() {
-		nextAccountID = 0;
+		nextAccountID = 7000001;
 	}
 	
 	public boolean addAccount(Account account) {
-		if(notYetFull()) {
 			account.setId(nextAccountID);
-			accounts[nextAccountID]=account;
+			accounts.put(nextAccountID, account);
 			System.out.println("Your account number is " + nextAccountID);
 			
 			nextAccountID++;
-			
 			return true;
-		}
-		else {
-			return false;
-		}
 	}
 	
 	public Account getAccount(int number) {
-		return accounts[number-1];
+		return accounts.get(number);
 	}
 	
 	public boolean notYetFull() {
-		return nextAccountID < accounts.length;
+		return nextAccountID < accounts.size();
 	}
 
 }
