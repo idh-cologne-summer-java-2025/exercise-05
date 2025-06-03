642aa74a9c8bb9689c3e21301baeac2b5183dcdf
karim <karim@air-von-karim>
2025-06-03 23:26:48 +0200

    mit Aufgabe 2

--- src/idh/java/ATMDemo.java
+++ src/idh/java/ATMDemo.java
@@ -2,24 +2,19 @@ package idh.java;
 
 public class ATMDemo {
 
-	public static void main(String[] args) {
-		Bank bank = new Bank();
-		
-		Account a1 = new Account(5000);
-		// TODO: a1.setPassword(...)
-		bank.addAccount(a1);
-		
-		Account a2 = new Account(5030);
-		// TODO: a2.setPassword(...)
-		bank.addAccount(a2);
-		
-		Account a3 = new Account(5030);
-		// TODO: a2.setPassword(...)
-		bank.addAccount(a3);
-		
-		ATM atm = new ATM(bank);
-		atm.run();
-		
-	}
+    public static void main(String[] args) {
+        Bank bank = new Bank();
 
+        Account a1 = new Account(5000, "1234");
+        bank.addAccount(a1);
+
+        Account a2 = new Account(5030, "5678");
+        bank.addAccount(a2);
+
+        Account a3 = new Account(5030, "9999");
+        bank.addAccount(a3);
+
+        ATM atm = new ATM(bank);
+        atm.run();
+    }
 }
